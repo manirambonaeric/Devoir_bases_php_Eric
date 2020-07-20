43f2ff9c6036fd38517ba0d24ecfc6d24f45e535
somali <oliviersomali@gmail.com>
2020-07-20 17:26:55 +0200

    affichage de resultat avec la fonction echo si c'est true ou si c'est faux

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // les variables
-$a=2;
+/*$a=2;
 $b=4;
 function multiplication($a,$b)
 {
@@ -9,16 +9,19 @@ function multiplication($a,$b)
     $b=$a*$b;
     echo $b;
 }
-multiplication(2,4);
+multiplication(2,4);*/
+
 //les booleens
 $poids=20;
-if(poids>=15)
+if($poids >=15)
 {
     $a=true;
+    echo 'le poids est valide';
 }
 else
 {
     $a=false;
+    echo 'le poids n\' est pa valide';
 }
 
 
